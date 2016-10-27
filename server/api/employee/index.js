109c3eae632bab915679cce93430347cb801de4f
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-10-27 11:26:41 -0500

    Add ES6 import and authentication to employee endpoint controller

--- server/api/employee/index.js
+++ server/api/employee/index.js
@@ -1,16 +1,18 @@
 /**
  * Created by cleverjam on 10/26/16.
  */
-var express = require("express");
-var controller = require("./employee.controller");
-var router = express.Router();
+import {Router} from 'express';
+import * as controller from './employee.controller';
+import * as auth from '../../auth/auth.service';
+
+var router = new Router();
 
 require("./seed");
 
-router.get("/", controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
+router.get("/", auth.hasRole('admin'), controller.index);
+router.get('/:id', auth.hasRole('admin'), controller.show);
+router.post('/', auth.hasRole('admin'), controller.create);
+router.put('/:id', auth.hasRole('admin'), controller.update);
 
 
 module.exports = router;
