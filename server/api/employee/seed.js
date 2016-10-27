47bc0db533f146156952954fbc788a242ff90ff4
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-10-27 11:14:42 -0500

    update employee seeder to spec

--- server/api/employee/seed.js
+++ server/api/employee/seed.js
@@ -6,287 +6,423 @@ import Employee from './employee.model';
 
 Employee.find({}).remove()
     .then(function () {
-        Employee.create({
-            "first_name": "Jeffrey",
-            "last_name": "Patterson",
-            "clients": [
-                "Aimbo",
-                "Edgeclub"
+        Employee.create([{
+            "first_name": "Adam",
+            "last_name": "Griffin",
+            "client": [
+                "Mudo"
             ],
-            "skills": [
-                "Tcsh",
-                "IM",
-                "AED"
+            "skill": [
+                "DDA"
             ],
-            "recruiter": "Jeffrey Patterson",
-            "salary": 65161
+            "recruiter": "Arnold",
+            "placement_type": "project",
+            "salary": 99895,
+            "insurance": 1000,
+            "relocation": 1715,
+            "immigration": 8451,
+            "pay_vacation_cost": 7072,
+            "target_bill_rate": 104574,
+            "client_bill_pay": 83784,
+            "activate": false
         }, {
-            "first_name": "Jeffrey",
-            "last_name": "Meyer",
-            "clients": [
-                "Youbridge",
-                "Kwideo"
+            "first_name": "Patricia",
+            "last_name": "Bradley",
+            "client": [
+                "Flashdog",
+                "Dabtype",
+                "Topicshots"
             ],
-            "skills": [
-                "MDI",
-                "JCreator",
-                "Visual Basic"
+            "skill": [
+                "Clinical Development"
             ],
-            "recruiter": "Jeffrey Meyer",
-            "salary": 59132
+            "recruiter": "Ricky",
+            "placement_type": "part-time",
+            "salary": 80613,
+            "insurance": 611,
+            "relocation": 3653,
+            "immigration": 9005,
+            "pay_vacation_cost": 6629,
+            "target_bill_rate": 65990,
+            "client_bill_pay": 111757,
+            "activate": false
         }, {
-            "first_name": "Albert",
-            "last_name": "Kelly",
-            "clients": [
-                "Skimia",
-                "Babblestorm"
+            "first_name": "Kathryn",
+            "last_name": "Ramos",
+            "client": [
+                "Kazio"
             ],
-            "skills": [
-                "XTRACT",
-                "LBO",
-                "LDO"
+            "skill": [
+                "Screening"
             ],
-            "recruiter": "Albert Kelly",
-            "salary": 92726
+            "recruiter": "Bobby",
+            "placement_type": "part-time",
+            "salary": 91679,
+            "insurance": 352,
+            "relocation": 2516,
+            "immigration": 7159,
+            "pay_vacation_cost": 6361,
+            "target_bill_rate": 60021,
+            "client_bill_pay": 108495,
+            "activate": false
         }, {
-            "first_name": "Nicholas",
-            "last_name": "Stevens",
-            "clients": [
-                "Wikizz",
-                "Jaxbean"
+            "first_name": "Walter",
+            "last_name": "Diaz",
+            "client": [
+                "Skynoodle",
+                "Shufflester",
+                "Yodo"
             ],
-            "skills": [
-                "Customer Engagement",
-                "3Com NBX",
-                "Study Abroad Programs"
+            "skill": [
+                "JavaSE",
+                "Knowledge Sharing"
             ],
-            "recruiter": "Nicholas Stevens",
-            "salary": 92904
+            "recruiter": "Ricky",
+            "placement_type": "full-time",
+            "salary": 71558,
+            "insurance": 288,
+            "relocation": 4861,
+            "immigration": 7782,
+            "pay_vacation_cost": 8737,
+            "target_bill_rate": 89969,
+            "client_bill_pay": 106938,
+            "activate": false
         }, {
-            "first_name": "Louise",
-            "last_name": "Andrews",
-            "clients": [
-                "Mycat",
-                "Feedbug"
+            "first_name": "Clarence",
+            "last_name": "Coleman",
+            "client": [
+                "Latz",
+                "Wordify",
+                "Mycat"
             ],
-            "skills": [
-                "Teeth Whitening",
-                "JWICS",
-                "AMI"
+            "skill": [
+                "SAP Business ByDesign",
+                "DVD Authoring"
             ],
-            "recruiter": "Louise Andrews",
-            "salary": 82152
+            "recruiter": "Ricky",
+            "placement_type": "full-time",
+            "salary": 63776,
+            "insurance": 386,
+            "relocation": 3961,
+            "immigration": 7070,
+            "pay_vacation_cost": 6762,
+            "target_bill_rate": 106396,
+            "client_bill_pay": 84250,
+            "activate": true
         }, {
-            "first_name": "Douglas",
-            "last_name": "Parker",
-            "clients": [
-                "Cogibox",
-                "Cogidoo"
+            "first_name": "Carolyn",
+            "last_name": "Cruz",
+            "client": [
+                "Browsedrive",
+                "Youtags",
+                "Divape"
             ],
-            "skills": [
-                "TCM",
-                "Pumps",
-                "Chemical Engineering"
+            "skill": [
+                "OBIEE"
             ],
-            "recruiter": "Douglas Parker",
-            "salary": 62378
-        }, {
-            "first_name": "Shawn",
-            "last_name": "Hudson",
-            "clients": [
-                "Trunyx",
-                "Oloo"
-            ],
-            "skills": [
-                "Creative Writing",
-                "DNA Sequencing",
-                "Titration"
-            ],
-            "recruiter": "Shawn Hudson",
-            "salary": 72507
+            "recruiter": "Arnold",
+            "placement_type": "project",
+            "salary": 76511,
+            "insurance": 941,
+            "relocation": 1557,
+            "immigration": 8014,
+            "pay_vacation_cost": 6971,
+            "target_bill_rate": 103492,
+            "client_bill_pay": 101606,
+            "activate": false
         }, {
             "first_name": "Timothy",
-            "last_name": "Watson",
-            "clients": [
-                "Meembee",
-                "Browsetype"
+            "last_name": "Ross",
+            "client": [
+                "Brightdog",
+                "Thoughtstorm"
             ],
-            "skills": [
-                "UB04",
-                "Electric Utility",
-                "Nitrous Oxide"
+            "skill": [
+                "Lawson HRIS"
             ],
-            "recruiter": "Timothy Watson",
-            "salary": 86863
+            "recruiter": "Bobby",
+            "placement_type": "part-time",
+            "salary": 86177,
+            "insurance": 346,
+            "relocation": 1280,
+            "immigration": 5571,
+            "pay_vacation_cost": 5269,
+            "target_bill_rate": 64198,
+            "client_bill_pay": 97347,
+            "activate": false
         }, {
-            "first_name": "Phyllis",
-            "last_name": "Stone",
-            "clients": [
-                "Dynazzy",
-                "Skivee"
+            "first_name": "Paul",
+            "last_name": "Fuller",
+            "client": [
+                "Voolith"
             ],
-            "skills": [
-                "BSI Tax Factory",
-                "NCover",
-                "Petroleum Geology"
+            "skill": [
+                "UCC filings"
             ],
-            "recruiter": "Phyllis Stone",
-            "salary": 98703
-        }, {
-            "first_name": "Joyce",
-            "last_name": "Rice",
-            "clients": [
-                "Realcube",
-                "Bluezoom"
-            ],
-            "skills": [
-                "RS232",
-                "KML",
-                "GPRS"
-            ],
-            "recruiter": "Joyce Rice",
-            "salary": 77206
-        }, {
-            "first_name": "Joshua",
-            "last_name": "Riley",
-            "clients": [
-                "Oodoo",
-                "Izio"
-            ],
-            "skills": [
-                "Yard Signs",
-                "Outdoors",
-                "Young People"
-            ],
-            "recruiter": "Joshua Riley",
-            "salary": 96278
-        }, {
-            "first_name": "Edward",
-            "last_name": "Ramos",
-            "clients": [
-                "Fiveclub",
-                "Realmix"
-            ],
-            "skills": [
-                "CTAs",
-                "Literature",
-                "RFQ Development"
-            ],
-            "recruiter": "Edward Ramos",
-            "salary": 79924
+            "recruiter": "Rocky",
+            "placement_type": "project",
+            "salary": 73004,
+            "insurance": 613,
+            "relocation": 2500,
+            "immigration": 8485,
+            "pay_vacation_cost": 5729,
+            "target_bill_rate": 109610,
+            "client_bill_pay": 117599,
+            "activate": false
         }, {
             "first_name": "Joe",
-            "last_name": "Wallace",
-            "clients": [
-                "Linkbuzz",
-                "Riffpedia"
+            "last_name": "Walker",
+            "client": [
+                "Tavu",
+                "Gigaclub",
+                "Yodo"
             ],
-            "skills": [
-                "Group Dynamics",
-                "Financial Risk",
-                "Business Ethics"
+            "skill": [
+                "Start-ups",
+                "NPO"
             ],
-            "recruiter": "Joe Wallace",
-            "salary": 84281
+            "recruiter": "Ricky",
+            "placement_type": "project",
+            "salary": 56728,
+            "insurance": 264,
+            "relocation": 4818,
+            "immigration": 9962,
+            "pay_vacation_cost": 7896,
+            "target_bill_rate": 115543,
+            "client_bill_pay": 87657,
+            "activate": true
         }, {
-            "first_name": "Keith",
-            "last_name": "Robertson",
-            "clients": [
-                "Jaxworks",
-                "Brainverse"
+            "first_name": "Robert",
+            "last_name": "Washington",
+            "client": [
+                "Yodel",
+                "Devpoint"
             ],
-            "skills": [
-                "ABAP-OO",
-                "CGI scripts",
-                "DS4000"
+            "skill": [
+                "National Security",
+                "RPG III"
             ],
-            "recruiter": "Keith Robertson",
-            "salary": 56618
+            "recruiter": "Ricky",
+            "placement_type": "project",
+            "salary": 53474,
+            "insurance": 784,
+            "relocation": 3122,
+            "immigration": 5421,
+            "pay_vacation_cost": 7314,
+            "target_bill_rate": 118865,
+            "client_bill_pay": 97842,
+            "activate": true
         }, {
-            "first_name": "Sharon",
-            "last_name": "Stephens",
-            "clients": [
-                "Wordtune",
-                "Flashdog"
+            "first_name": "Tina",
+            "last_name": "Mccoy",
+            "client": [
+                "Cogibox",
+                "Reallinks"
             ],
-            "skills": [
-                "Video over IP",
-                "Mac OS X Server",
-                "Fitness"
+            "skill": [
+                "Automotive",
+                "DVD"
             ],
-            "recruiter": "Sharon Stephens",
-            "salary": 68003
+            "recruiter": "Bobby",
+            "placement_type": "project",
+            "salary": 97786,
+            "insurance": 346,
+            "relocation": 3054,
+            "immigration": 7653,
+            "pay_vacation_cost": 6292,
+            "target_bill_rate": 71390,
+            "client_bill_pay": 102582,
+            "activate": false
         }, {
-            "first_name": "Maria",
-            "last_name": "Moore",
-            "clients": [
-                "JumpXS",
-                "Brainverse"
+            "first_name": "Janet",
+            "last_name": "Bishop",
+            "client": [
+                "Rhybox",
+                "Skippad",
+                "Digitube"
             ],
-            "skills": [
-                "LPIC",
-                "Hazardous Materials",
-                "DLX"
+            "skill": [
+                "Lawson HRIS",
+                "Drupal"
             ],
-            "recruiter": "Maria Moore",
-            "salary": 87101
-        }, {
-            "first_name": "Sharon",
-            "last_name": "Peterson",
-            "clients": [
-                "Blogtag",
-                "Skyndu"
-            ],
-            "skills": [
-                "QS9000",
-                "WSUS",
-                "Aerial Photography"
-            ],
-            "recruiter": "Sharon Peterson",
-            "salary": 95926
+            "recruiter": "Bobby",
+            "placement_type": "full-time",
+            "salary": 86142,
+            "insurance": 511,
+            "relocation": 4280,
+            "immigration": 6432,
+            "pay_vacation_cost": 9992,
+            "target_bill_rate": 112173,
+            "client_bill_pay": 111484,
+            "activate": true
         }, {
             "first_name": "Bruce",
-            "last_name": "Garrett",
-            "clients": [
-                "Ozu",
-                "Topiclounge"
+            "last_name": "Palmer",
+            "client": [
+                "Brightbean",
+                "Mycat",
+                "Thoughtsphere"
             ],
-            "skills": [
-                "Wrongful Death",
-                "OTDR",
-                "Fluorescence Microscopy"
+            "skill": [
+                "SI",
+                "User Scenarios"
             ],
-            "recruiter": "Bruce Garrett",
-            "salary": 61292
+            "recruiter": "Bobby",
+            "placement_type": "project",
+            "salary": 97995,
+            "insurance": 166,
+            "relocation": 2262,
+            "immigration": 8097,
+            "pay_vacation_cost": 8519,
+            "target_bill_rate": 87305,
+            "client_bill_pay": 103588,
+            "activate": true
         }, {
-            "first_name": "Amy",
-            "last_name": "Perry",
-            "clients": [
-                "Reallinks",
-                "Kayveo"
+            "first_name": "Andrea",
+            "last_name": "Payne",
+            "client": [
+                "Yozio",
+                "Cogilith",
+                "Zooxo"
             ],
-            "skills": [
-                "OO Software Development",
-                "CMC Regulatory Affairs",
-                "Energy Efficiency"
+            "skill": [
+                "Type Approval"
             ],
-            "recruiter": "Amy Perry",
-            "salary": 85273
+            "recruiter": "Bobby",
+            "placement_type": "full-time",
+            "salary": 67533,
+            "insurance": 347,
+            "relocation": 3594,
+            "immigration": 7367,
+            "pay_vacation_cost": 7294,
+            "target_bill_rate": 103543,
+            "client_bill_pay": 117013,
+            "activate": true
         }, {
-            "first_name": "Katherine",
-            "last_name": "Perkins",
-            "clients": [
-                "Aimbo",
-                "Gabtune"
+            "first_name": "Bonnie",
+            "last_name": "Stevens",
+            "client": [
+                "Jabbersphere",
+                "Bubblemix",
+                "Ntag"
             ],
-            "skills": [
-                "NDDS",
-                "DFR",
-                "Urban Search &amp; Rescue"
+            "skill": [
+                "Occupational Therapy"
             ],
-            "recruiter": "Katherine Perkins",
-            "salary": 75288
-        })
+            "recruiter": "Ricky",
+            "placement_type": "part-time",
+            "salary": 59936,
+            "insurance": 765,
+            "relocation": 1429,
+            "immigration": 9515,
+            "pay_vacation_cost": 7467,
+            "target_bill_rate": 109375,
+            "client_bill_pay": 112658,
+            "activate": true
+        }, {
+            "first_name": "Mark",
+            "last_name": "Brown",
+            "client": [
+                "Wordtune",
+                "Vipe"
+            ],
+            "skill": [
+                "MCMS"
+            ],
+            "recruiter": "Ricky",
+            "placement_type": "full-time",
+            "salary": 68094,
+            "insurance": 587,
+            "relocation": 2416,
+            "immigration": 6640,
+            "pay_vacation_cost": 7488,
+            "target_bill_rate": 91212,
+            "client_bill_pay": 97064,
+            "activate": true
+        }, {
+            "first_name": "Scott",
+            "last_name": "Collins",
+            "client": [
+                "Wordpedia"
+            ],
+            "skill": [
+                "Golf",
+                "MGCP"
+            ],
+            "recruiter": "Bobby",
+            "placement_type": "project",
+            "salary": 77796,
+            "insurance": 261,
+            "relocation": 2466,
+            "immigration": 8087,
+            "pay_vacation_cost": 8603,
+            "target_bill_rate": 81912,
+            "client_bill_pay": 107068,
+            "activate": true
+        }, {
+            "first_name": "Cheryl",
+            "last_name": "Frazier",
+            "client": [
+                "Avavee",
+                "Jabberbean",
+                "Linkbridge"
+            ],
+            "skill": [
+                "Offshore Oil",
+                "EHR"
+            ],
+            "recruiter": "Ricky",
+            "placement_type": "project",
+            "salary": 57943,
+            "insurance": 869,
+            "relocation": 1907,
+            "immigration": 9840,
+            "pay_vacation_cost": 7441,
+            "target_bill_rate": 94913,
+            "client_bill_pay": 117448,
+            "activate": true
+        }, {
+            "first_name": "Joshua",
+            "last_name": "Garcia",
+            "client": [
+                "Twimbo"
+            ],
+            "skill": [
+                "TSCM",
+                "WCDMA"
+            ],
+            "recruiter": "Arnold",
+            "placement_type": "full-time",
+            "salary": 91008,
+            "insurance": 100,
+            "relocation": 3023,
+            "immigration": 7956,
+            "pay_vacation_cost": 7069,
+            "target_bill_rate": 116538,
+            "client_bill_pay": 118646,
+            "activate": true
+        }, {
+            "first_name": "Rachel",
+            "last_name": "Robertson",
+            "client": [
+                "Jayo",
+                "Brainverse"
+            ],
+            "skill": [
+                "Substance Abuse Prevention",
+                "Working Abroad"
+            ],
+            "recruiter": "Ricky",
+            "placement_type": "part-time",
+            "salary": 88339,
+            "insurance": 872,
+            "relocation": 3646,
+            "immigration": 8663,
+            "pay_vacation_cost": 7846,
+            "target_bill_rate": 108254,
+            "client_bill_pay": 104163,
+            "activate": true
+        }])
             .then(function () {
                 console.log('finished populating employees');
             });
