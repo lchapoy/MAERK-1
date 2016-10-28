81b444999ffa4ad3014b76a11b31362133b7d711
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-10-28 13:16:22 -0500

    Add employee dialog controller

--- client/app/employee/employee-dialog/employee.dialog.controller.js
+++ client/app/employee/employee-dialog/employee.dialog.controller.js
@@ -3,9 +3,37 @@
  */
 (function () {
     class DialogController {
-        constructor(EmployeeResource, employee){
-            this.context = angular.isArray(employee) ? "Edit" : "Add";
 
+        constructor(EmployeeResource, employee, $mdDialog, $mdConstant) {
+            this.context = angular.isArray(employee) ? "Edit" : "Add";
+            //chip separators.
+            this.keys = [$mdConstant.KEY_CODE.ENTER, $mdConstant.KEY_CODE.COMMA, 186];
+            this.allClients = this.loadClients();
+            this.client = [];
+            this.skill = [];
+            this.close = function () {
+                $mdDialog.cancel();
+            }
+        }
+
+
+        querySearch(criteria) {
+            return this.allClients.filter(this.createFilterFor(criteria));
+        }
+
+        createFilterFor(query) {
+            var lowercaseQuery = angular.lowercase(query);
+            return function filterFn(client) {
+                return (client.name.indexOf(lowercaseQuery) != -1);
+            };
+
+        }
+
+        loadClients() {
+            //ClientResource here!
+            return ["verizon", "deloitte", "intuit", "boy scouts"].map(function (c, i) {
+                return {name: c}
+            });
         }
     }
 
