d98f15a6615956d17e5ee1240d9b1562e4848ad0
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-10-28 14:53:09 -0500

    Add employee dialog controller behavior

--- client/app/employee/employee-dialog/employee.dialog.controller.js
+++ client/app/employee/employee-dialog/employee.dialog.controller.js
@@ -4,19 +4,33 @@
 (function () {
     class DialogController {
 
-        constructor(EmployeeResource, employee, $mdDialog, $mdConstant) {
+        constructor(employee, $mdDialog, $mdConstant) {
             this.context = angular.isArray(employee) ? "Edit" : "Add";
             //chip separators.
             this.keys = [$mdConstant.KEY_CODE.ENTER, $mdConstant.KEY_CODE.COMMA, 186];
             this.allClients = this.loadClients();
-            this.client = [];
-            this.skill = [];
+            this.emp = {};
+            this.emp.client = [];
+            this.emp.skill = [];
+
+            this.submit = function () {
+                this.emp.client = this.fixClient(this.emp.client);
+                $mdDialog.hide(this.emp);
+            };
             this.close = function () {
                 $mdDialog.cancel();
-            }
+            };
+
         }
 
-
+        //returns an array with client names [string]
+        fixClient(client) {
+            let result = [];
+            client.forEach((e)=> {
+                result.push(e.name);
+            });
+            return result;
+        }
         querySearch(criteria) {
             return this.allClients.filter(this.createFilterFor(criteria));
         }
