7a8f6767c1184533666fe4de0e9b6934fe0558dd
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-11-02 12:09:17 -0500

    Add chart directive containers

---
+++ client/components/chart/chart.controller.js
@@ -0,0 +1,3 @@
+/**
+ * Created by cleverjam on 11/2/16.
+ */
---
+++ client/components/chart/chart.directive.js
@@ -0,0 +1,9 @@
+/**
+ * Created by cleverjam on 11/2/16.
+ */
+angular.module('maerkApp')
+    .directive('chart', ()=> ({
+        templateUrl:'components/chart/chart.html',
+        restrict:'E',
+        controller:''
+    }));
