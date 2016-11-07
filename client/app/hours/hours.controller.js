fe506237ac599f553714677e9caca1fb64a25f99
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-11-07 14:39:09 -0600

    Filter employees by start date to not show before they were hired

--- client/app/hours/hours.controller.js
+++ client/app/hours/hours.controller.js
@@ -3,14 +3,35 @@
  */
 class HoursController {
 
-    constructor(months, $mdEditDialog, EmployeeResource, ReportResource, $q, reportData) {
+    constructor(months, $mdEditDialog, employeeList, $q, reportData) {
         this.orderVal = 'client';
         this.page = 1;
-        this.limit = 5;
+        this.limit = 10;
+        this.reportData = reportData;
+        this.year = reportData.year;
         this.months = months;
-        console.log(reportData);
+        this.closed = reportData.closed+11;
+        this.selection = {
+            year: 2016,
+            month: 0
+        };
+        //table loading.
+        var deferred = $q.defer();
+        this.promise = deferred.promise;
 
-        //STARTUP
+        //************************************************
+        // ******* UPDATING EMPLOYEE LIST ****************
+        //************************************************
+
+        this.update = (year = this.selection.year, month = this.selection.month) => {  //year and month must be Number
+            console.log(year, month);
+            if (this.reportData[months[this.closed]].length == 0) {
+                this.initMonth(employeeList, 2016, month);
+                setTimeout(() => {
+                    deferred.resolve();
+                }, 1000)
+            }
+        };
 
         //************************************************
         // ******* EDIT HOURS AND OFFSET *****************
@@ -55,6 +76,20 @@ class HoursController {
                 })
             })
         };
+
+        this.update(2016, 0);
+    }
+
+    initMonth(employeeList, year, month) {
+        var emp = [];
+        employeeList.forEach((employee)=> {
+            if (new Date(employee['start_date']).getYear() <= year) {
+                if (new Date(employee['start_date']).getMonth() <= month) {
+                    emp.push(employee);
+                }
+            }
+        });
+        this.employees = emp;
     }
 }
 
