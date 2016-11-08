574c25353392f5fd394adeebb852203a9405b866
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-11-08 11:37:58 -0600

    Update loading employees for report's month

--- client/app/hours/hours.controller.js
+++ client/app/hours/hours.controller.js
@@ -12,19 +12,22 @@ class HoursController {
         this.limit = 10;
         this.months = months;
         this.employees = [];  //will be initialized with loadMonth();
+        this.closed = reportData.closed;
 
         //controller logic
         this.q = $q;
         this.ReportResource = ReportResource;
         this.employeeList = employeeList;
         this.reportData = reportData;
-        this.selected = new Date(this.reportData.year, reportData.closed);
+        this.mdDialog = $mdDialog;
+        this.selected = new Date(this.reportData.year, this.closed);
         //initialize
         this.promise = this.loadMonth().then(()=> {
             console.log(this.reportData);
         });
     }
 
+    //load a month after the selected date has been updated!
     loadMonth() {
         var deferred = this.q.defer();
         var year = this.selected.getYear() + 1900;
@@ -41,16 +44,30 @@ class HoursController {
                     this.employees.push(employee);
                 }
             });
-        };
 
-        if (this.reportData.year === year) { //selected year same as loaded report year.
-            loadEmployees();
-            deferred.resolve();
-        } else {
-            return this.ReportResource.createYear(year).then((r)=> {
-                loadEmployees();
-                this.reportData = r; //get new year's report data
+            // Updating the reportData's month if no employees found in it:
+            let updateObj = {
+                _id: this.reportData._id,
+            };
+            updateObj[this.months[month]] = this.employees;
+            this.ReportResource.updateMonth(updateObj).then((reportData)=> {
+                this.reportData = reportData;
+                deferred.resolve();
             })
+
+        };
+        //are the employees already loaded??
+        if (this.reportData.year === year && this.reportData[this.months[month]].length === 0) { //selected year same as loaded report year.
+            loadEmployees();
+        } else if (this.reportData.year !== year) {
+            console.log('creating year and loading employees');
+            return this.ReportResource.createYear(year).then((r)=> {
+                this.reportData = r; //get new year's report data
+                loadEmployees();
+            })
+        } else if (this.reportData[this.months[month]].length > 0) {
+            this.employees = this.reportData[this.months[month]];
+            deferred.resolve();
         }
         return deferred.promise;
     }
@@ -60,8 +77,29 @@ class HoursController {
             this.selected.setYear(year);
         if (month)
             this.selected.setMonth(month);
-        this.loadMonth();
+        this.promise = this.loadMonth()
     }
+
+    close(e) {
+        var confirm = this.mdDialog.confirm()
+            .title("Are you sure you want to close this month")
+            .textContent(`There are a total of ${this.employees.length} employees in this month's registration.`)
+            .ariaLabel("Close monthly registration")
+            .targetEvent(e)
+            .ok("Close")
+            .cancel("Cancel");
+        this.promise = this.mdDialog.show(confirm);
+        this.promise.then(()=> {
+            this.closed = this.closed + 1;
+            return this.ReportResource.closeMonth(this.closed - 1);
+        }).then((d)=> {
+            console.log(d);
+            this.reportData = d;
+        });
+        this.promise.catch(()=> {
+            console.log("NOOOO");
+        })
+    };
 }
 
 angular.module('maerkApp')
