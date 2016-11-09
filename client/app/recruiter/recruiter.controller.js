2ec1b0cabba98fe54b7cb44be7bed09bed3afb48
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-11-09 14:49:51 -0600

    Add initial recruiter state files and update index.html

---
+++ client/app/recruiter/recruiter.controller.js
@@ -0,0 +1,10 @@
+/**
+ * Created by cleverjam on 11/9/16.
+ */
+class RecruiterController {
+    constructor() {
+        this.test = '1...2...3...'
+    }
+}
+angular.module('maerkApp')
+    .controller('RecruiterController', RecruiterController)
--- client/app/recruiter/recruiter.js
+++ client/app/recruiter/recruiter.js
@@ -1,17 +1,14 @@
 /**
  * Created by cleverjam on 11/9/16.
  */
-/**
- * Created by cleverjam on 11/1/16.
- */
 
 angular.module('maerkApp')
     .config(function($stateProvider) {
         $stateProvider.state('main.recruiter',{
             authenticate: true,
             url:'/recruiter',
-            templateUrl: '',
-            controller: '',
-            controllerAs: ''
+            templateUrl: 'app/recruiter/recruiter.html',
+            controller: 'RecruiterController',
+            controllerAs: 'vm'
         });
     });
