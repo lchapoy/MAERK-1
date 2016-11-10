44fd3576b1e228873f6a1d62b89356ab973b12ea
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-11-10 11:21:18 -0600

    Add recruiter controller logic to load add edit dialog

--- client/app/recruiter/recruiter.controller.js
+++ client/app/recruiter/recruiter.controller.js
@@ -2,15 +2,39 @@
  * Created by cleverjam on 11/9/16.
  */
 class RecruiterController {
-    constructor(recruiterList) {
+    constructor(recruiterList, $mdDialog, RecruiterResource) {
         this.Math = window.Math;
+        this.mdDialog = $mdDialog;
+        this.RecruiterResource = RecruiterResource;
         this.page = 1;
         this.orderVal = "first_name";
         this.limit = 5;
         this.filterBy='first_name';
         this.selected = [];
         this.recruiters = recruiterList;
-        console.log(recruiterList)
+    }
+
+    showAddDialog($event, selected) {
+        this.mdDialog.show({
+            controller: "RecruiterDialogController",
+            controllerAs: "vm",
+            templateUrl: 'app/recruiter/recruiter-dialog/recruiter.dialog.html',
+            parent: angular.element(document.body),
+            targetEvent: $event,
+            clickOutsideToClose: true,
+            fullscreen: true,
+            locals: {
+                recruiter: selected ? selected[0] : null
+            }
+        })
+            .then(rec => {
+                if (!rec._id) {
+                    this.RecruiterResource.create(rec);
+                }
+                else {
+                    this.RecruiterResource.update(rec);
+                }
+            });
     }
 }
 angular.module('maerkApp')
