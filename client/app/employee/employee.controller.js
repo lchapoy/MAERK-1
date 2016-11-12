189a8eed3f2e75be3da78f54ef09f9063afb9e54
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-11-11 20:46:00 -0600

    update employee controller

--- client/app/employee/employee.controller.js
+++ client/app/employee/employee.controller.js
@@ -1,8 +1,8 @@
 /**
  * Created by cleverjam on 10/25/16.
  */
-angular.module("maerkApp")
-    .controller("EmployeeController", function ($timeout, employeeList, recruiterList, $mdDialog, EmployeeResource) {
+class EmployeeController {
+    constructor($timeout, employeeList, recruiterList, $mdDialog, EmployeeResource) {
         this.page = 1;
         this.orderVal = "first_name";
         this.limit = 5;
@@ -10,78 +10,87 @@ angular.module("maerkApp")
         this.selected = [];
         this.employees = employeeList;
         this.recruiters = recruiterList;
+        this.mdDialog = $mdDialog;
+        this.EmployeeResource = EmployeeResource;
+    }
 
-        this.delete = function ($event, employees) {
-            //noinspection JSUnresolvedFunction
-            var prompt = $mdDialog.confirm()
-                .title("Are you sure you want to DELETE Employees")
-                .textContent(`You are deleting (${employees.length}) Employees. This action CANNOT be reversed.`)
-                .ariaLabel("Active status change confirmation")
-                .targetEvent($event)
-                .ok("DELETE")
-                .cancel("Cancel");
-            $mdDialog.show(prompt).then(()=> {
-                this.selected.forEach((employee) => {
-                    EmployeeResource.delete(employee)
-                        .then((updated)=> {
-                            this.employees = updated;
-                            this.selected = [];
-                        });
+    deleteEmployee($event, employees) {
+        //noinspection JSUnresolvedFunction
+        var prompt = this.mdDialog.confirm()
+            .title("Are you sure you want to DELETE Employees")
+            .textContent(`You are deleting (${employees.length}) Employees. This action CANNOT be reversed.`)
+            .ariaLabel("Active status change confirmation")
+            .targetEvent($event)
+            .ok("DELETE")
+            .cancel("Cancel");
+        this.mdDialog.show(prompt).then(()=> {
+            this.selected.forEach((employee) => {
+                this.EmployeeResource.delete(employee)
+                    .then((updated)=> {
+                        this.employees = updated;
+                        this.selected = [];
                 });
-            }, ()=> {
-                console.log("no employees deleted.")
-            })
-        };
+            });
+        }, ()=> {
+            console.log("no employees deleted.")
+        })
+    }
 
-        this.toggleActive = ($event,val) => {
-            //noinspection JSUnresolvedFunction
-            var prompt = $mdDialog.confirm()
-                .title("Please confirm active status change")
-                .textContent(`You are changing the status for (${this.selected.length}) employees to ${val?"active":"inactive"}`)
-                .ariaLabel("Active status change confirmation")
-                .targetEvent($event)
-                .ok("Change Status")
-                .cancel("Cancel");
-            $mdDialog.show(prompt).then(()=>{
-                this.selected.forEach((employee)=> {
-                    if (employee.activate !== val) {
-                        console.info("Toggling active status for:", employee["first_name"]);
-                        employee.activate = val;
-                        employee.$save();
-                    }
-                })
-            },()=>{
-                console.log("no status changed.")
-            })
-        };
-        this.showAddDialog = function ($event, selected) {
-            $mdDialog.show({
-                controller: "DialogController",
-                controllerAs: "vm",
-                templateUrl: "app/employee/employee-dialog/employee.dialog.html",
-                parent: angular.element(document.body),
-                targetEvent: $event,
-                clickOutsideToClose: true,
-                fullscreen: true,
-                locals: {
-                    employee: selected ? selected[0] : null,
-                    recruiters: this.recruiters
-                }
-            }).then((emp)=> {
-                if (!emp._id){
-                    EmployeeResource.create(emp);
-                }
-                else {
-                    EmployeeResource.update(emp);
+    toggleActive($event, val) {
+        //noinspection JSUnresolvedFunction
+        var prompt = this.mdDialog.confirm()
+            .title("Please confirm active status change")
+            .textContent(`You are changing the status for (${this.selected.length}) employees to ${val ? "active" : "inactive"}`)
+            .ariaLabel("Active status change confirmation")
+            .targetEvent($event)
+            .ok("Change Status")
+            .cancel("Cancel");
+        this.mdDialog.show(prompt).then(()=> {
+            this.selected.forEach((employee)=> {
+                if (employee.activate !== val) {
+                    console.info("Toggling active status for:", employee["first_name"]);
+                    employee.activate = val;
+                    employee.$save();
                 }
             })
-        };
+        }, ()=> {
+            console.log("no status changed.")
+        })
+    }
 
-        var originatorEv;
-        this.openMenu = function ($mdOpenMenu, ev) {
-            originatorEv = ev;
-            $mdOpenMenu(ev);
-        };
+    showAddDialog($event, selected) {
+        this.mdDialog.show({
+            controller: "DialogController",
+            controllerAs: "vm",
+            templateUrl: "app/employee/employee-dialog/employee.dialog.html",
+            parent: angular.element(document.body),
+            targetEvent: $event,
+            clickOutsideToClose: true,
+            fullscreen: true,
+            locals: {
+                employee: selected ? selected[0] : null,
+                recruiters: this.recruiters
+            }
+        }).then((emp)=> {
+            if (!emp._id) {
+                this.EmployeeResource.create(emp);
+            }
+            else {
+                this.EmployeeResource.update(emp);
+            }
+        })
+    }
+
+    openMenu($mdOpenMenu, ev) {
+        this.originatorEv = ev;
+        $mdOpenMenu(ev);
+    }
+}
+
+angular.module('maerkApp')
+    .controller('EmployeeController', EmployeeController);
 
 
-    });
+
+
+
